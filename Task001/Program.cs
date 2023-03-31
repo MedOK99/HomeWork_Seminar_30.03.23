951d5d8ad2b9992331a6ef588ee42d7914b67efb
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-31 10:06:24 +0300

    Решил задачу с координатами

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -9,3 +9,30 @@ A (7,-5, 0); B (1,-1,9) -> 11.53    */
 
 Console.Clear();
 
+Console.WriteLine("Введите координаты X1:");
+
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y1:");
+
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z1:");
+
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X2:");
+
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y2:");
+
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z2:");
+
+int z2 = int.Parse(Console.ReadLine());
+
+double answer = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2)); 
+
+Console.WriteLine(Math.Round(answer, 2));
