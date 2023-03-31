53b9393cc668713b79e57b37427f952dcca56331
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-31 17:40:16 +0300

    Стрёмное решение палиндрома

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Напишите программу, которая принимает на вход 
+﻿/* Задача 19
+Напишите программу, которая принимает на вход 
 пятизначное число и проверяет, является ли оно палиндромом.
 
 14212 -> нет
@@ -9,4 +10,54 @@
 
 Console.Clear();
 
+Console.WriteLine("Введите пятизначное число: ");
 
+int num = int.Parse(Console.ReadLine());
+
+
+
+int p = num % 10;   // последнее число
+int d = num / 10000;  // первое число
+int r = num - (d * 10000);
+int d2 = r / 1000;  // второе число
+int r2 = r - d2 * 1000;   //  
+int d3 = r2 / 100;
+int r3 = r2 - d3*100;
+int p2 = r3 /10;  // предпоследнее число
+
+
+if (num > 9999 && num < 100000)
+{
+  if (p == d && p2 == d2)
+  {
+    Console.WriteLine($"Число {num} является палиндромом");
+  }
+else
+Console.WriteLine($"Число {num} НЕ является палиндромом");
+
+}
+else
+Console.WriteLine("Вы ввели НЕ верное число!");
+
+
+
+
+/*
+int num, r, sum = 0, t;
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000)
+{
+  for (t = num; num != 0; num = num / 10)
+  {
+    r = num % 10;
+    sum = sum * 10 + r;
+  }
+  if (t == sum)
+    Console.Write($"{t} является палиндромом.");
+  else
+    Console.Write($"{t} не является палиндромом.");
+}
+else
+  Console.Write("Введите пятизначное число! ");
+*/
